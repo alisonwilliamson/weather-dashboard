ac9f1f1083f4c9a5f3842d4c8a0b7545b2632bae
Allie <williamson.alisonj@gmail.com>
2020-07-24 04:45:44 -0400

    function to get searched city's current weather

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,45 @@
 
 let cityList = [];
-let API = "1d1dbf3b5675b2bb385f9922fc8feae7";
-
-// stores searched cities in local storage
-function storeCities() {
-    localStorage.setItem("cities", JSON.stringify(cityList));
-}
+let API = "1bb4961b1e82421db6b174ff95e35d71";
 
 
-// adds last searched city to cityList-group
-function addCity(){
+
+
+// stores searched cities in local storage----unecessary???
+// function storeCities() {
+//     localStorage.setItem("cities", JSON.stringify(cityList));
+// }
+
+
+
+// adds last searched city to list-group
+function createList(){
     $(".cityList").empty();
     cityList.forEach(function(city) {
-        $(".cityList").prepend($("<button class='cityList-group-item cityList-group-item-action citySearches' city-data='"+city+"'>"+city+"</button>"));
+        $(".cityList").prepend($('<button class="list-group-item list-group-item-action cityButton" data-city="'+city+'">'+city+'</button>'));
     })
 }
+
+// gets current weather for selected city and calls uv index function
+function currentWeather(city, API) {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid="+API;
+    
+    // api call to get searched city's current weather
+    $.ajax({
+        url: weatherURL,
+        method: "GET"
+    }).then(function (data) {
+        // appends weather data 
+        $(".todaysWeather").append(
+            `<div class="row ml-1">
+                <h3 class="mr-3">${data.name} (${(new Date(1000 * data.dt).getUTCMonth()) + 1}/${(new Date(1000 * data.dt).getUTCDate())}/${new Date(1000 * data.dt).getUTCFullYear()})</h3>
+                <img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png">
+            </div>`
+        )
+        $(".todaysWeather").append("<p>Temperature: "+data.main.temp+" &degF</p>")
+        $(".todaysWeather").append("<p>Humidity: "+data.main.humidity+" %</p>")
+        $(".todaysWeather").append("<p>Wind: "+data.wind.speed+" mph</p>")
+    })
+
+}
+
